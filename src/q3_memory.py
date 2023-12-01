4abe4f1b3a2de8321c12bf16544868c92edc9ee6
ManuelVoiden <manuelvoiden@gmail.com>
2023-12-01 12:30:38 -0500

    Added the content of Q3 Memory and Team for both the challenge .ipynb and the .py files

--- src/q3_memory.py
+++ src/q3_memory.py
@@ -1,4 +1,34 @@
+import pandas as pd
+import json
+import time
+import datetime
+import memory_profiler
+import re
 from typing import List, Tuple
 
+from datetime import datetime
+from memory_profiler import memory_usage
+from collections import defaultdict, Counter
+
 def q3_memory(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    mention_counter = Counter()
+    mention_pattern = re.compile(r'@(\w+)')
+
+    # Process file line by line
+    with open(file_path, 'r') as file:
+        for line in file:
+            tweet = json.loads(line)
+            mentions = mention_pattern.findall(tweet.get('content', ''))
+            for mention in mentions:
+                mention_counter[mention] += 1
+
+    # Get the top 10 mentions
+    top_mentions = mention_counter.most_common(10)
+
+    return top_mentions
+
+file_path = "./data/farmers-protest-tweets-2021-2-4.json"
+
+#Executing q2_memory and printing the result
+top_emojis = q3_memory(file_path)
+print(top_emojis)
--- src/q3_time.py
+++ src/q3_time.py
@@ -1,4 +1,37 @@
+import pandas as pd
+import json
+import time
+import datetime
+import memory_profiler
+import re
 from typing import List, Tuple
 
+from datetime import datetime
+from memory_profiler import memory_usage
+from collections import defaultdict, Counter
+
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    # Read the entire file into a Pandas DataFrame
+    df = pd.read_json(file_path, lines=True)
+
+    # Regular expression to extract mentions
+    mention_pattern = re.compile(r'@(\w+)')
+
+    # Extract mentions and explode the DataFrame
+    df['mentions'] = df['content'].str.findall(mention_pattern)
+    df_exploded = df.explode('mentions')
+
+    # Count and get the top 10 mentions
+    top_mentions = df_exploded['mentions'].value_counts().head(10)
+
+    # Convert to list of tuples
+    top_mentions_output = list(top_mentions.items())
+
+    return top_mentions_output
+
+
+file_path = "./data/farmers-protest-tweets-2021-2-4.json"
+
+#Executing q2_time and printing the result
+top_emojis = q3_time(file_path)
+print(top_emojis)
